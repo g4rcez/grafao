d60177592b90a967415a194fcdc887957f1a806c
rochavianna <rocha.vianna12@gmail.com>
2017-12-06 22:58:59 -0200

    Dijkistra Controller, Kruskal Algo + Controller

---
+++ SourceCode/src/java/controller/DijkstraController.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import model.Grafo;
+import model.No;
+import model.algoritmos.Dijkistra;
+
+/**
+ *
+ * @author garcez
+ */
+@WebServlet(name = "DijkstraController", urlPatterns = {"/dijkstra"})
+public class DijkstraController extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        Grafo grafo = (Grafo) request.getSession().getAttribute("grafo");
+        Dijkistra dijkstra = new Dijkistra(grafo);
+        String nomeNoOrigem = request.getParameter("noOrigem");
+        String nomeNoDestino = request.getParameter("noDestino");
+        No noOrigem = grafo.getNo(nomeNoOrigem);
+        if ("todosNos".equals(nomeNoDestino)) {
+            List<List<No>> caminhosDaOrigem = new ArrayList<>();
+            grafo.getNos().stream().map((no) -> dijkstra.calcularDijkstra(noOrigem, no)).forEachOrdered((caminho) -> {
+                caminhosDaOrigem.add(caminho);
+            });
+            request.setAttribute("caminhosDaOrigem", caminhosDaOrigem);
+        } else {
+            No noDestino = grafo.getNo(nomeNoDestino);
+            List<No> caminho = dijkstra.calcularDijkstra(noOrigem, noDestino);
+            request.setAttribute("caminho", caminho);
+            request.setAttribute("noDestino", nomeNoDestino);
+        }
+        request.setAttribute("nomeNoOrigem", nomeNoOrigem);
+        getServletContext().getRequestDispatcher("/dijkstra.jsp").forward(request, response);
+
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ SourceCode/src/java/model/algoritmos/Kruskal.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.algoritmos;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import model.Aresta;
+import model.ArestaComparator;
+import model.Grafo;
+import model.No;
+
+/**
+ *
+ * @author garcez
+ */
+public class Kruskal {
+
+    public Kruskal() {
+    }
+    
+
+    public Grafo algoritmoDeKruskal(Grafo grafo) {
+        Grafo subGrafo = new Grafo(grafo.getId(), grafo.getTipo(), grafo.getTipoAresta(), grafo.getNos(), new ArrayList<>());
+        List<Aresta> arestasDoGrafo = new ArrayList<>();
+        arestasDoGrafo.addAll(grafo.getArestas());
+        Map<String, Integer> nosDoGrafo = new HashMap<>();
+        int numeroVertices = grafo.getNos().size(), i = 0;
+        int[][] matrizComponentes = new int[2][grafo.getNos().size()];
+        Collections.sort(arestasDoGrafo, new ArestaComparator());
+        for (No no : grafo.getNos()) {
+            nosDoGrafo.put(no.getId(), i);
+            matrizComponentes[0][i] = i;
+            matrizComponentes[1][i] = i;
+            i++;
+        }
+        while ((subGrafo.getArestas().size() <= numeroVertices - 1) && !arestasDoGrafo.isEmpty()) {
+            Aresta aresta = arestasDoGrafo.get(0);
+            arestasDoGrafo.remove(aresta);
+            int componente_u = this.getComponenteKruskal(matrizComponentes, nosDoGrafo.get(aresta.getOrigem().getId()));
+            int componente_v = this.getComponenteKruskal(matrizComponentes, nosDoGrafo.get(aresta.getDestino().getId()));
+            if (componente_u != componente_v) {
+                this.mergeComponenteKruskal(matrizComponentes, componente_u, componente_v);
+                subGrafo.adicionarAresta(aresta);
+            }
+        }
+        return subGrafo;
+    }
+
+    public int getComponenteKruskal(int[][] matriz, int componente) {
+        int valor = matriz[1][componente];
+        return valor;
+    }
+
+    public int[][] mergeComponenteKruskal(int[][] matriz, int componente_1, int componente_2) {
+        int i, componenteATrocada = matriz[1][componente_2];
+        for (i = 0; i < matriz[0].length; i++) {
+            if (matriz[1][i] == componenteATrocada) {
+                matriz[1][i] = matriz[1][componente_1];
+            }
+        }
+        return matriz;
+    }
+}
